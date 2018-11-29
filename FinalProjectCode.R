123b46b0a43174d65e8609bd395e9651e10424d0
tbillman <trb9259@uncw.edu>
2018-11-28 20:15:28 -0500

    Post-Exam Update

---
+++ FinalProjectCode.R
@@ -0,0 +1,13 @@
+##### STT 420 Final Project #####
+#####   Logistic Incedence  #####
+##### Authors: Thomas Billman, Danielle Chaung, James Royal #####
+
+##### Population Simulation #####
+gender  <- c(.48,.52) #m, f
+age <- c(.19, .13, .26, .25, .16) #0-17, 18-24, 25-44, 45-64, 65+
+race <- c(.817, .14, .016, .006) #W, B, Asian, Indian
+mental <- c(.8,.2) #n, y
+substance <- c(.906, .094) #n,y
+perscription <- c(.8, .2) #n, y
+hosp <- c(.927, .073) #n, y
+rural <- c(.978, .022) #n, y
